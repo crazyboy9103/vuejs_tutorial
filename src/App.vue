a554bd4955bd5c7781b280dbc1f5c309fd333585
crazyboy9103 <gil9103@gmail.com>
2022-07-16 15:57:03 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <app-message></app-message>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- <style scoped> 는 App.vue 파일에만 스타일을 적용해라 라는 의미--> 
+<style>
+</style>
---
+++ src/Input.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-bind:value="msg" v-on:input="changeMessage"></input>
+        <p> {{ message }}</p>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: [
+            'msg'
+        ],
+        data() {
+            return {
+                message: ''
+            }
+        },
+        methods: {
+            changeMessage(event) {
+                this.message = event.target.value;
+                this.$emit('messageChanged', this.message);
+                // $ is built in method
+            }
+        }
+    }
+</script>
